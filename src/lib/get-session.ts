3846412c6f7c44008eefd967e7ff6a5a9c502173
kamrul-code-maker <kamrulcoder2@gmail.com>
2025-10-01 22:45:27 +0600

    Request deduplication with React.cache

--- src/lib/get-session.ts
+++ src/lib/get-session.ts
@@ -1,10 +1,15 @@
 import { headers } from "next/headers";
 import { auth } from "./auth";
+import { cache } from "react";
 
+// export async function getServerSession() {
+//     return await auth.api.getSession({headers: await headers()})
+// }
 
+export const getServerSession = cache(async() => {
+    console.log("getServerSession called ");
 
-export async function getServerSession() {
-    return await auth.api.getSession({headers: await headers()})
-}
+    return await auth.api.getSession({headers:await headers()})
+})
 
 
